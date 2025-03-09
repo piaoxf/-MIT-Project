abe26a4703a9ce9db0f4fe473db43fdc71c1bf42
Eric <76478067+piaoxf@users.noreply.github.com>
2025-03-09 23:07:32 +0900

    1 ok

--- Machine Learning 6.86.x/mnist/part1/linear_regression.py
+++ Machine Learning 6.86.x/mnist/part1/linear_regression.py
@@ -15,8 +15,10 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    d = X.shape[1]
+    I = np.eye(d)
+    theta = np.linalg.solve(X.T @ X + lambda_factor * I, X.T @ Y)
+    return theta
 
 ### Functions which are already complete, for you to use ###
 
