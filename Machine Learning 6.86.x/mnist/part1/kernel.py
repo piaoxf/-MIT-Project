86ca660c7684dc86e1749b039113338db50f681e
Eric <76478067+piaoxf@users.noreply.github.com>
2025-03-12 19:51:40 +0900

    kernel complete

--- Machine Learning 6.86.x/mnist/part1/kernel.py
+++ Machine Learning 6.86.x/mnist/part1/kernel.py
@@ -19,8 +19,10 @@ def polynomial_kernel(X, Y, c, p):
         Returns:
             kernel_matrix - (n, m) Numpy array containing the kernel matrix
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    kernel_matrix = np.dot(X, Y.T) + c
+    kernel_matrix = np.power(kernel_matrix, p)
+    
+    return kernel_matrix
 
 
 
@@ -38,5 +40,21 @@ def rbf_kernel(X, Y, gamma):
         Returns:
             kernel_matrix - (n, m) Numpy array containing the kernel matrix
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    # (n,m)のカーネル行列を初期化
+    kernel_matrix = np.zeros((X.shape[0], Y.shape[0]))
+    for i in range(X.shape[0]):
+        for j in range(Y.shape[0]):
+            # ユークリッド距離の2乗を計算し、ガウスカーネルを計算
+            kernel_matrix[i, j] = np.exp(-gamma * np.linalg.norm(X[i] - Y[j]) ** 2)
+    return kernel_matrix
+
+    # より効率的な実装
+    # x_norm = np.sum(X ** 2, axis=1).reshape(-1, 1) # 各 x_iのノルムの2乗(n, 1)
+    # y_norm = np.sum(Y ** 2, axis=1).reshape(1, -1) # 各 y_iのノルムの2乗(1, m)
+
+    # # ユークリッド距離の2乗を計算 ||X-Y||^2 = ||X||^2 + ||Y||^2 - 2<X, Y>
+    # dist = x_norm + y_norm - 2 * np.dot(X, Y.T) 
+
+    # # ガウスカーネルを計算
+    # kernel_matrix = np.exp(-ganma * dist)
+    # return kernel_matrix
