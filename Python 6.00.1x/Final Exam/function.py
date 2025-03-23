c11f2757a8bb54cdaa3e714a2e9f02170a1ea185
Eric <76478067+piaoxf@users.noreply.github.com>
2025-03-23 11:59:17 +0900

    final 6 ok

--- Python 6.00.1x/Final Exam/function.py
+++ Python 6.00.1x/Final Exam/function.py
@@ -81,6 +81,80 @@ def longest_run(L):
             result = sum(increasing[inc_start])
     return result
 
+def uniqueValues(aDict):
+    '''
+    aDict: a dictionary
+    returns: a sorted list of keys that map to unique aDict values, empty list if none
+    '''
+    # Your code here
+    result = []
+    values = list(aDict.values())
+    for x,y in aDict.items():
+        if values.count(y) <= 1:
+            result.append(x)
+    result.sort()
+    return result
+
+## DO NOT MODIFY THE IMPLEMENTATION OF THE Person CLASS ##
+class Person(object):
+    def __init__(self, name):
+        #create a person with name name
+        self.name = name
+        try:
+            firstBlank = name.rindex(' ')
+            self.lastName = name[firstBlank+1:]
+        except:
+            self.lastName = name
+        self.age = None
+    def getLastName(self):
+        #return self's last name
+        return self.lastName
+    def setAge(self, age):
+        #assumes age is an int greater than 0
+        #sets self's age to age (in years)
+        self.age = age
+    def getAge(self):
+        #assumes that self's age has been set
+        #returns self's current age in years
+        if self.age == None:
+            raise ValueError
+        return self.age
+    def __lt__(self, other):
+        #return True if self's name is lexicographically less
+        #than other's name, and False otherwise
+        if self.lastName == other.lastName:
+            return self.name < other.name
+        return self.lastName < other.lastName
+    def __str__(self):
+        #return self's name
+        return self.name
+        
+class USResident(Person):
+    """ 
+    A Person who resides in the US.
+    """
+    def __init__(self, name, status):
+        """ 
+        Initializes a Person object. A USResident object inherits 
+        from Person and has one additional attribute:
+        status: a string, one of "citizen", "legal_resident", "illegal_resident"
+        Raises a ValueError if status is not one of those 3 strings
+        """
+        # Write your code here
+        super().__init__(name)
+
+        valid_status = ["citizen", "legal_resident", "illegal_resident"]
+        if status in valid_status:
+            self.status = status
+        else:
+            raise ValueError
+        
+    def getStatus(self):
+        """
+        Returns the status
+        """
+        # Write your code here
+        return self.status
     
     
 
--- Python 6.00.1x/Final Exam/test.py
+++ Python 6.00.1x/Final Exam/test.py
@@ -12,3 +12,14 @@ L = [10, 4, 3, 8, 3, 4, 5, 7, 7, 2]
 fun.longest_run(L)
 L = [5, 4, 10]
 fun.longest_run(L)
+
+# uniqueValues(aDict)
+aDict = {1: 1, 3: 2, 6: 0, 7: 0, 8: 4, 10: 0}
+print(fun.uniqueValues(aDict))
+aDict = {1: 1, 2: 1, 3: 1}
+print(fun.uniqueValues(aDict))
+
+# USResident
+a = fun.USResident('Tim Beaver', 'citizen')
+print(a.getStatus())
+b = fun.USResident('Tim Horton', 'non-resident')
